c7917fc46d219b37148fcfb64cef96686850b666
Chaz <8712755+chazzhou@users.noreply.github.com>
2022-01-08 20:30:27 -0600

    Generalized item display

--- set/src/App.js
+++ set/src/App.js
@@ -2,19 +2,19 @@ import React from 'react';
 import './App.css';
 import Header from './components/Header.js'
 import Footer from './components/Footer.js'
-import TopList from './components/Top.js'
+import ItemList from './components/ItemDisplay'
 
 const clothes = {
   "tops": {
       "Strong Together" : {
-        "name" : "Strong Together",
+        "name" : "Strong Together T-Shirt",
         "brand" : "Cinq à Sept",
         "color" : ["grey"],
         "type" : "T-Shirt",
         "image" : "https://image.s5a.com/is/image/saks/0400012787086_HEATHERGREYWHITE"
       },
       "Nora Floral Boxy" : {
-        "name" : "Nora Floral Boxy",
+        "name" : "Nora Floral Boxy T-Shirt",
         "brand" : "Leset",
         "color" : ["white", "blue"],
         "type" : "T-Shirt",
@@ -27,22 +27,65 @@ const clothes = {
         "type" : "T-Shirt",
         "image" : "https://image.s5a.com/is/image/saks/0400014628136_SAND"
       }
+  },
+  "bottoms": {
+    "Le Jane Two-Tone Jeans" : {
+      "name" : "Le Jane Two-Tone Jeans",
+      "brand" : "Frame",
+      "color" : ["blue"],
+      "type" : "Jeans",
+      "image" : "https://image.s5a.com/is/image/saks/0400015380808_DELLNOIR"
+    },
+    "Trevor High-Rise Patchwork Jeans" : {
+      "name" : "Trevor High-Rise Patchwork Jeans",
+      "brand" : "Jonathan Simkhai Standard",
+      "color" : ["blue", "white"],
+      "type" : "T-Shirt",
+      "image" : "https://image.s5a.com/is/image/saks/0400015593348_SILVERLAKE"
+    },
+  },
+  "shoes": {
+    "Leather Lug Sole Chelsea Boots" : {
+      "name" : "Leather Lug Sole Chelsea Boots",
+      "brand" : "Prada",
+      "color" : ["black"],
+      "type" : "Boot",
+      "image" : "https://image.s5a.com/is/image/saks/0400014408655_NERO"
+    },
+    "Women's Suede Oversized Sneakers" : {
+      "name" : "Women's Suede Oversized Sneakers",
+      "brand" : "Alexandar McQueen",
+      "color" : ["white", "black"],
+      "type" : "Sneaker",
+      "image" : "https://image.s5a.com/is/image/saks/0400010174918_WHITEBLACK?"
+    },
+    "Marmont Leather Thong Sandals With Double G" : {
+      "name" : "Marmont Leather Thong Sandals With Double G",
+      "brand" : "Gucci",
+      "color" : ["black", "gold"],
+      "type" : "Sandal",
+      "image" : "https://image.s5a.com/is/image/saks/0400096071331_BLACK?"
   }
+}
 };
-
-
 
 function App() {
   return (
     <div>
       <Header />
-      <TopList tops={ clothes.tops } />
-      <div id = "bottom">
-        bottom
-      </div>
-      <div id = "shoes">
-        shoes
-      </div>
+      <div class="dropdown item-selector">
+        <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          Dropdown button
+        </button>
+      <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+      <a class="dropdown-item" href="#">Action</a>
+      <a class="dropdown-item" href="#">Another action</a>
+      <a class="dropdown-item" href="#">Something else here</a>
+  </div>
+</div>
+      <ItemList items={ clothes.tops } />
+      <ItemList items={ clothes.bottoms } />
+      <ItemList items={ clothes.shoes } />
       <Footer />
     </div>
   )
--- set/src/components/ItemDisplay.js
+++ set/src/components/ItemDisplay.js
@@ -1,31 +1,31 @@
 import React from 'react';
 
-const TopList = ({ tops }) => (
+const ItemList = ({ items }) => (
     <div className="container">
         <div className="album py-5">
         <div className="row">
-            { Object.values(tops).map(tops => <Top top={ tops } />) }
+            { Object.values(items).map(items => <Item item={ items } />) }
         </div>
         </div>
     </div>
 );
 
-const Top = ({ top }) => (
+const Item = ({ item }) => (
     <div className="col-md-4">
     <div class="card mb-4 box-shadow">
-        <img class="card-img-top" src={ top.image } alt={ top.name }/>
+        <img class="card-img-top" src={ item.image } alt={ item.name }/>
             <div class="card-body">
-                <p class="card-text">{ top.name } by { top.brand }</p>
+                <p class="card-text">{ item.name } by { item.brand }</p>
                     <div class="d-flex justify-content-between align-items-center">
                     <div class="btn-group">
                         <button type="button" class="btn btn-sm btn-outline-secondary">View</button>
                         <button type="button" class="btn btn-sm btn-outline-secondary">Edit</button>
                     </div>
-                    <small class="text-muted">{ top.type }</small>
+                    <small class="text-muted">{ item.type }</small>
                     </div>
                 </div>
             </div>
     </div>
 );
 
-export default TopList;
+export default ItemList;
