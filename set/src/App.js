8b58d8995f2e34ddabae5a901197fced67595184
Chaz <8712755+chazzhou@users.noreply.github.com>
2022-01-08 19:54:25 -0600

    Added top displays

--- set/src/App.js
+++ set/src/App.js
@@ -2,14 +2,39 @@ import React from 'react';
 import './App.css';
 import Header from './components/Header.js'
 import Footer from './components/Footer.js'
+import TopList from './components/Top.js'
+
+const clothes = {
+  "tops": {
+      "Strong Together" : {
+        "name" : "Strong Together",
+        "brand" : "Cinq à Sept",
+        "color" : ["grey"],
+        "type" : "T-Shirt",
+        "image" : "https://image.s5a.com/is/image/saks/0400012787086_HEATHERGREYWHITE"
+      },
+      "Nora Floral Boxy" : {
+        "name" : "Nora Floral Boxy",
+        "brand" : "Leset",
+        "color" : ["white", "blue"],
+        "type" : "T-Shirt",
+        "image" : "https://image.s5a.com/is/image/saks/0400014573744_WHITEBLUE"
+      },
+      "Dive Cold-Shoulder Tee" : {
+        "name" : "Dive Cold-Shoulder Tee",
+        "brand" : "LNA",
+        "color" : ["sand"],
+        "type" : "T-Shirt",
+        "image" : "https://image.s5a.com/is/image/saks/0400014628136_SAND"
+      }
+  }
+};
 
 function App() {
   return (
     <div>
       <Header />
-      <div id = "top">
-        top
-      </div>
+      <TopList tops={ clothes.tops } />
       <div id = "bottom">
         bottom
       </div>
