f06087ed8ca14cc706f47aee38021ad742df0d36
kelly00su <56286184+kelly00su@users.noreply.github.com>
2022-01-08 19:55:44 -0600

    styling header and footer

--- set/src/App.js
+++ set/src/App.js
@@ -3,6 +3,8 @@ import './App.css';
 import Header from './components/Header.js'
 import Footer from './components/Footer.js'
 
+
+
 function App() {
   return (
     <div>
--- set/src/components/Footer.js
+++ set/src/components/Footer.js
@@ -6,12 +6,21 @@ import HomeIcon from '@material-ui/icons/Home';
 import AddIcon from '@material-ui/icons/Add';
 import PersonIcon from '@material-ui/icons/Person';
 
+const styles = {
+    footerIcon: {
+      fill: "#1c3580",
+      margin: 10,
+      width: 40,
+      height: 40,
+    },
+  };
+
 const Footer = () =>  (
     <div className="footer"> 
         <nav className="nav nav-pills nav-fill">
-            <a className="nav-item nav-link" href="#"><HomeIcon style={{fill: "#1c3580"}}/></a>
-            <a className="nav-item nav-link" href="#"><AddIcon style={{fill: "#1c3580"}}/></a>
-            <a className="nav-item nav-link" href="#"><PersonIcon style={{fill: "#1c3580"}}/></a>
+            <a className="nav-item nav-link" href="#"><HomeIcon style={styles.footerIcon}/></a>
+            <a className="nav-item nav-link" href="#"><AddIcon style={styles.footerIcon}/></a>
+            <a className="nav-item nav-link" href="#"><PersonIcon style={styles.footerIcon}/></a>
         </nav>
     </div>
 );
