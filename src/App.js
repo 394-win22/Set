8317fe090190a24182e716fd2f802def0d84735f
Chaz <8712755+chazzhou@users.noreply.github.com>
2022-01-12 20:43:35 -0600

    Update navbar

--- src/App.js
+++ src/App.js
@@ -6,18 +6,20 @@ import RecommendPage from './pages/RecommendPage';
 import NewItemPage from './pages/NewItemPage';
 import UserPage from './pages/UserPage';
 import LoginPage from './pages/LoginPage';
+import Footer from './components/Footer'
 
 function App() {
   return (
     <div>
       <BrowserRouter>
         <Routes>
+          <Route exact path="/login" element={<LoginPage />} />
           <Route path="/" element={<ClosetPage />} />
           <Route path="/recommend" element={<RecommendPage />} />
           <Route path="/additem" element={<NewItemPage/>} />
           <Route path="/user" element={<UserPage />} />
-          <Route path="/login" element={<LoginPage />} />
         </Routes>
+        <Footer />
       </BrowserRouter>
     </div>
   )
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,27 +1,39 @@
 import React from 'react';
 import './Footer.css';
 import { Nav, NavItem} from 'reactstrap';
-import { NavLink } from 'react-router-dom';
-import CheckroomIcon from '@mui/icons-material/Checkroom';
-import AssistantIcon from '@mui/icons-material/Assistant';
-import AddCircleIcon from '@mui/icons-material/AddCircle';
-import AccountCircleIcon from '@mui/icons-material/AccountCircle';
+import { NavLink, Link, useLocation } from 'react-router-dom';
+import {Checkroom, Assistant, AddCircle, AccountCircle} from '@mui/icons-material';
+import { SpeedDial, SpeedDialIcon, SpeedDialAction } from '@mui/material';
+
+import Box from '@mui/material/Box';
+import CssBaseline from '@mui/material/CssBaseline';
+import BottomNavigation from '@mui/material/BottomNavigation';
+import BottomNavigationAction from '@mui/material/BottomNavigationAction';
+import RestoreIcon from '@mui/icons-material/Restore';
+import FavoriteIcon from '@mui/icons-material/Favorite';
+import ArchiveIcon from '@mui/icons-material/Archive';
+import Paper from '@mui/material/Paper';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import ListItemAvatar from '@mui/material/ListItemAvatar';
+import ListItemText from '@mui/material/ListItemText';
+import Avatar from '@mui/material/Avatar';
 
 const tabs = [{
   route: "/",
-  icon: CheckroomIcon,
+  icon: Checkroom,
   label: "Closet"
 },{
   route: "/recommend",
-  icon: AssistantIcon,
+  icon: Assistant,
   label: "Recommend"
 },{
   route: "/additem",
-  icon: AddCircleIcon,
+  icon: AddCircle,
   label: "Add"
 },{
   route: "/user",
-  icon: AccountCircleIcon,
+  icon: AccountCircle,
   label: "User"
 }]
 
@@ -32,35 +44,66 @@ const styles = {
     }
   };
 
-const Footer = () => (
-  <>
-    <div className="footer-margin"></div>
-    <nav className="fixed-bottom navbar-light bottom-tab-nav footer" role="navigation">
-        <Nav className="w-100">
-          <div className=" d-flex flex-row justify-content-around w-100">
-            {
-              tabs.map((tab, index) => {
-                const Icon = tab.icon;
-                return (
-                <NavItem key={`tab-${index}`}>
-                  <NavLink to={tab.route} className="nav-link bottom-nav-link" style={({ isActive }) => {
-                      return {
-                        display: "block",
-                        color: isActive ? "#1c3580" : ""
-                      };
-                    }}>
-                    <div className="row d-flex flex-column justify-content-center align-items-center">
-                      <Icon style={styles.footerIcon} />
-                      <div className="bottom-tab-label">{tab.label}</div>
-                    </div>
-                  </NavLink>
-                </NavItem>
-              );})
-            }
-          </div>
-        </Nav>
-    </nav>
-  </> 
-);
+// const Footer = () => (
+//   <>
+//     <div className="footer-margin"></div>
+//     <nav className="fixed-bottom navbar-light bottom-tab-nav footer" role="navigation">
+//         <Nav className="w-100">
+//           <div className=" d-flex flex-row justify-content-around w-100">
+//             {
+//               tabs.map((tab, index) => {
+//                 const Icon = tab.icon;
+//                 return (
+//                 <NavItem key={`tab-${index}`}>
+//                   <NavLink to={tab.route} className="nav-link bottom-nav-link" style={({ isActive }) => {
+//                       return {
+//                         display: "block",
+//                         color: isActive ? "#1c3580" : ""
+//                       };
+//                     }}>
+//                     <div className="row d-flex flex-column justify-content-center align-items-center">
+//                       <Icon style={styles.footerIcon} />
+//                       <div className="bottom-tab-label">{tab.label}</div>
+//                     </div>
+//                   </NavLink>
+//                 </NavItem>
+//               );})
+//             }
+//           </div>
+//         </Nav>
+//     </nav>
+//   </> 
+// );
+
+function Footer() {
+  const [value, setValue] = React.useState(0);
+  
+  const ref = React.useRef(null);
+
+  const { pathname } = useLocation();
+  if (pathname === "/login") return null;
+
+  return (
+    <Box sx={{ pb: 7 }}>
+      <CssBaseline />
+      <Paper sx={{ position: 'fixed', bottom: 0, left: 0, right: 0}} elevation={4}>
+        <BottomNavigation
+          showLabels
+          value={value}
+          onChange={(event, newValue) => {
+            setValue(newValue);
+          }}
+        >
+          {tabs.map((tab, index) => {
+            const Icon = tab.icon;
+            return (
+              <BottomNavigationAction key={`tab-${index}`} component={Link} to={tab.route} label={tab.label} icon={<Icon />} />
+            );}
+          )}
+        </BottomNavigation>
+      </Paper>
+    </Box>
+  );
+}
 
 export default Footer;
