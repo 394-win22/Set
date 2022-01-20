785ff3ecf5e6be4cc227b181bb0d094e1148397a
Chaz <8712755+chazzhou@users.noreply.github.com>
2022-01-19 22:02:05 -0600

    Slider, stepper, and drag and drop

--- src/pages/NewItemPage.js
+++ src/pages/NewItemPage.js
@@ -1,34 +1,57 @@
-import React, { useState } from 'react';
-import Header from '../components/Header'
-import { storage } from '../utilities/firebase'
+import React, { useState } from "react";
+import Header from "../components/Header";
+import { storage } from "../utilities/firebase";
 import { ref, uploadBytes } from "firebase/storage";
+import HorizontalLinearStepper from "../components/Stepper";
+import { Col, Row, Card } from "react-bootstrap";
+import IOSSlider from "../components/IOSSlider";
+import { DropzoneArea } from 'material-ui-dropzone';
 
 const NewItemPage = () => {
-    const [file, setFile] = useState(null);
-    const [url, setURL] = useState("");
+  const [file, setFile] = useState(null);
+  const [url, setURL] = useState("");
 
-    function handleChange(e) {
-        setFile(e.target.files[0]);
-    }
+  function handleChange(e) {
+    setFile(e.target.files[0]);
+  }
 
-    function handleUpload(e) {
-        e.preventDefault();
-        const storageRef = ref(storage, `/images/${file.name}`);
-        uploadBytes(storageRef, file).then((snapshot) => {
-            console.log('Uploaded a blob or file!');
-        });
-    }
+  function handleUpload(e) {
+    e.preventDefault();
+    const storageRef = ref(storage, `/images/${file.name}`);
+    uploadBytes(storageRef, file).then((snapshot) => {
+      console.log("Uploaded a blob or file!");
+    });
+  }
 
-    return (
+  return (
     <div>
-        <Header />
-        <form onSubmit={handleUpload}>
-            <input type="file" onChange={handleChange} />
-            <button disabled={!file}>upload to firebase</button>
-        </form>
-        <img src={url} alt="" />
+      <Header />
+      <div>
+        <DropzoneArea
+            acceptedFiles={['image/*']}
+            dropzoneText={"Drag and drop an image here or click"}
+            onChange={(files) => console.log('Files:', files)}
+        />
+      </div>
+      <form onSubmit={handleUpload}>
+        <div className="file-upload-box">
+          <Card style={{ width: "auto" }}>
+            <input
+              className="form-control form-control-lg"
+              type="file"
+              onChange={handleChange}
+            />
+          </Card>
+        </div>
+        <button disabled={!file}>upload to firebase</button>
+      </form>
+      <img src={url} alt="" />
+      <IOSSlider />
+      <IOSSlider />
+      <IOSSlider />
+      <HorizontalLinearStepper />
     </div>
-    );
-}
+  );
+};
 
 export default NewItemPage;
