a08fc03884e95af299f2752857951ad91d61b069
William Xenakis <william.xenakis@gmail.com>
2022-01-25 23:27:47 -0600

    Click on outfit to enlarge and see outfit modal

--- src/pages/OutfitsPage.js
+++ src/pages/OutfitsPage.js
@@ -1,7 +1,8 @@
-import React from "react";
+import {React, useState} from "react";
 import Header from "../components/Header";
 import imgAccessories from "../images/img_acc.png";
 import { DeleteOutlined } from '@ant-design/icons'
+import { Button, Card, Col, Modal, Row } from "react-bootstrap";
 
 import {
 	useData,
@@ -13,6 +14,9 @@ import {
 } from "../utilities/firebase.js";
 
 const Outfit = ({ outfit, id, index }) => {
+
+	const [showModal, setShowModal] = useState(false);
+
 	const [top, loadingTop, errorTop] = useData(
 		getClothingItem("Tops", userId, outfit["Tops"]),
 		getAllData
@@ -41,65 +45,136 @@ const Outfit = ({ outfit, id, index }) => {
 	}
 
 	return (
-		<div className="col-6 col-sm-4 col-md-3 py-3">
-			<div className="card mb-4">
-				<div>
-					<div className="row">
-						<div className="col-6">
-							<img width="100%" src={top.image} alt={top.name} />
+		<>
+			<div className="col-6 col-sm-4 col-md-3 py-3" onClick={() => {
+				setShowModal(true)
+			}
+				}>
+				<div className="card mb-4">
+					<div>
+						<div className="row">
+							<div className="col-6">
+								<img width="100%" src={top.image} alt={top.name} />
+							</div>
+							<div className="col-6">
+							{accessories == null ? (
+								<img
+									width="100%"
+									src={imgAccessories}
+									alt="placeholderAccessory"
+								/>
+							) : (
+								<img
+									width="100%"
+									src={accessories.image}
+									alt={accessories.name}
+								/>
+							)}
+							</div>
 						</div>
-						<div className="col-6">
-						{accessories == null ? (
-							<img
-								width="100%"
-								src={imgAccessories}
-								alt="placeholderAccessory"
-							/>
-						) : (
-							<img
-								width="100%"
-								src={accessories.image}
-								alt={accessories.name}
-							/>
-						)}
+						<div className="row">
+							<div className="col-6">
+								<img
+									width="100%"
+									src={bottom.image}
+									alt={bottom.name}
+								/>
+							</div>
+							<div className="col-6">
+								<img
+									width="100%"
+									src={shoes.image}
+									alt={shoes.name}
+								/>
+							</div>
 						</div>
 					</div>
-					<div className="row">
-						<div className="col-6">
-							<img
-								width="100%"
-								src={bottom.image}
-								alt={bottom.name}
-							/>
-						</div>
-						<div className="col-6">
-							<img
-								width="100%"
-								src={shoes.image}
-								alt={shoes.name}
-							/>
-						</div>
-					</div>
-				</div>
 
-				<div className="card-body">
-					<p className="card-text">{`${outfit["Name"]} ${index + 1}`}</p>
-					<div className="d-flex justify-content-between align-items-center">
-						<div className="btn-group">
-							<button
-								type="button"
-								className="btn btn-sm btn-outline-secondary"
-								onClick={(ev) => {
-									removeOutfit()
-								}}
-							>
-								<DeleteOutlined />
-							</button>
+					<div className="card-body">
+						<p className="card-text">{`${outfit["Name"]} ${index + 1}`}</p>
+						<div className="d-flex justify-content-between align-items-center">
+							<div className="btn-group">
+								<button
+									type="button"
+									className="btn btn-sm btn-outline-secondary"
+									onClick={(ev) => {
+										removeOutfit()
+									}}
+								>
+									<DeleteOutlined />
+								</button>
+							</div>
 						</div>
 					</div>
 				</div>
 			</div>
-		</div>
+			<OutfitModal 
+				show={showModal}
+				onHide={() => setShowModal(false)}
+				shoes={shoes}
+				top={top}
+				accessories={accessories}
+				bottom={bottom}
+			/>
+		</>
+	);
+};
+
+const OutfitModal = (props) => {
+	return (
+		<Modal
+			show={props.show}
+			onHide={props.onHide}
+			size="lg"
+			aria-labelledby="contained-modal-title-vcenter"
+			centered
+		>
+			<Modal.Header closeButton></Modal.Header>
+			<Modal.Body>
+				<div className="col-6 col-sm-4 col-md-3 py-3">
+					<div className="card mb-4">
+						<div>
+							<div className="row">
+								<div className="col-6">
+									<img width="100%" src={props.top.image} alt={props.top.name} />
+								</div>
+								<div className="col-6">
+								{props.accessories == null ? (
+									<img
+										width="100%"
+										src={imgAccessories}
+										alt="placeholderAccessory"
+									/>
+								) : (
+									<img
+										width="100%"
+										src={props.accessories.image}
+										alt={props.accessories.name}
+									/>
+								)}
+								</div>
+							</div>
+							<div className="row">
+								<div className="col-6">
+									<img
+										width="100%"
+										src={props.bottom.image}
+										alt={props.bottom.name}
+									/>
+								</div>
+								<div className="col-6">
+									<img
+										width="100%"
+										src={props.shoes.image}
+										alt={props.shoes.name}
+									/>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</Modal.Body>
+		</Modal>
 	);
 };
 
